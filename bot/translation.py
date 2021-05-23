8ceaaccaf235cfe21caad82449562be361cee48e
itmearavind <71639579+itmearavind@users.noreply.github.com>
2021-05-23 09:19:13 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -6,11 +6,10 @@ class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
-
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Works only for @Tamil_LinkzZ 🤪 its ordee from my boss @its_me_Aravind   .
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@JNS_BOTS 
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +42,18 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@JNS_BOTS
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> TAMIL HD Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/JINTONS">JINTO NS</a></i></b>
+
+<b>➥ Boss</b> : <b><i><a href="https://t.me/its_me_Aravind">ARAVIND</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Support channel</b> : <i><a href="https://t.me/jns_bots">ＪƝ⟆ ᗷ〇Ƭ⟆</a></i>
 """
